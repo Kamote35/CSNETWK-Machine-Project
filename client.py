64562d16a728b2f4e1c7bccaa76fe6b457ee6be8
s18sayatjc <john_christian_sayat@dlsu.edu.ph>
2024-12-06 19:09:39 +0800

    added all command input and few error handling

--- client.py
+++ client.py
@@ -1,15 +1,27 @@
 import socket
+import os
 
 HEADER = 64
 SERVER_PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "/leave"
+REQUEST_COMMAND = "/?"
+DIRECTORY = "/dir"
 SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER_ADDRESS, SERVER_PORT)
 CONNECT_MESSAGE = (f"/join {SERVER_ADDRESS} {SERVER_PORT}")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+
+def inputsyntax():
+    print("\nHere are the following input commands:")
+    print("(1) Connect to server type: /join <server_address> <server_port>")
+    print("(2) Leave server: /leave")
+    print("(3) Register Username/Handle: /register <name>")
+    print("(4) Send file to server: /store <filename>")
+    print("(5) Request directory file list: /dir")
+    print("(6) Fetch a file from a server: /get <filename>")
+    print("(7) Request command help: /?")
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -19,20 +31,84 @@ def send(msg):
     client.send(send_length)
     client.send(message)
 
-# send(input("Please input a messag/command: "))
+def send_file(filename):
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            file_content = file.read()
+            send(f"/store {filename}")
+            send(file_content)
+            print(f"File '{filename}' sent to the server.")
+    else:
+        print(f"File '{filename}' does not exist.")
 
-print("To connect to server type: /join <server_address> <server_port>")
-print("")
-client_message = input("Please input a message/command: ")
+def fetch_file(filename):
+    send(f"/get {filename}")
+    msg_length = client.recv(HEADER).decode(FORMAT)
+    msg_length = int(msg_length)
+    file_content = client.recv(msg_length).decode(FORMAT)
+    with open(filename, 'w') as file:
+        file.write(file_content)
+    print(f"File '{filename}' fetched from the server.")
 
-if client_message == CONNECT_MESSAGE:
-    send(client_message)
-    print(f"Connected to succesfully to the File Exchange Server {SERVER_ADDRESS}!")
+def directory_list():
+    send("/dir")
+    msg_length = client.recv(HEADER).decode(FORMAT)
+    msg_length = int(msg_length)
+    files_list = client.recv(msg_length).decode(FORMAT)
+    print("Directory Listing:")
+    print(files_list)
 
-    while client_message != DISCONNECT_MESSAGE:
+# send(input("Please input a message/command: "))
+
+def main():
+    print("Welcome to the client section! To start, join the server first by inputting the following command: ")
+    print("(1) Connect to server type: /join <server_address> <server_port>")
+
+    connect = False
+
+    while not connect:
         client_message = input("Please input a message/command: ")
-        send(client_message)
 
-if client_message == DISCONNECT_MESSAGE:
-    send(client_message)
-    print(f"Disconnected successfully from File Exchange Server: {SERVER_ADDRESS}!")
+        if client_message == CONNECT_MESSAGE:
+            try:
+                client.connect(ADDR)
+                send(client_message)
+                print(f"Connected to succesfully to the File Exchange Server {SERVER_ADDRESS}!")
+                connect = True
+            except Exception as e:
+                print(f"Error connecting to the IP: {e}")
+        elif client_message == DISCONNECT_MESSAGE:
+            print("You are not connected. Please connect to the server first.")
+        else:
+            print(f"Error: Connection to the Server has failed! Please check IP Address and Port Number.")
+                
+        
+    while True:
+        client_message = input("Please input a message/command: ")
+        if client_message == REQUEST_COMMAND:
+            inputsyntax()
+        elif client_message.startswith("/store"):
+            filename = client_message.split(" ")[1]
+            send_file(filename)
+        elif client_message.startswith("/get"):
+            filename = client_message.split(" ")[1]
+            fetch_file(filename)
+        elif client_message == DIRECTORY: # /dir
+            directory_list()
+        elif client_message.startswith("/register"):
+            send(client_message)
+            msg_length = client.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                response = client.recv(msg_length).decode(FORMAT)
+                print(response)
+                continue
+        elif client_message == DISCONNECT_MESSAGE:
+            send(client_message)
+            print(f"Disconnected successfully from File Exchange Server: {SERVER_ADDRESS}!")
+        else:
+            send(client_message)
+
+    
+
+main()
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import os
+from datetime import datetime
 
 # Initialize the server
 HEADER = 64
@@ -8,16 +10,24 @@ SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER_ADDRESS, SERVER_PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "/leave"
-CONNECT_MESSAGE = (f"/join {SERVER_ADDRESS} {SERVER_PORT}")
-# print(server_address)
+DIRECTORY = "/dir"
+CONNECT_MESSAGE = (f"/join {SERVER_ADDRESS} {SERVER_PORT}") 
 
+#print(server_address)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+#dictionary to store client username
+client_alias = {}
+
+#Getting the timestamp
+currenttime = datetime.now()
+format_time = currenttime.strftime("%Y-%m-%d %I:%M:%S")
+
+
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
-
-
+    name = False
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
@@ -27,8 +37,54 @@ def handle_client(conn, addr):
             if msg == DISCONNECT_MESSAGE:
                 connected = False
                 print(f"[DISCONNECTED] {addr} has disconnected")
-
-        print(f"[{addr}] {msg}")
+            elif msg.startswith("/register"): #user inputs /register
+                alias = msg.split(" ")[1]
+                if alias in client_alias.values():
+                    error_msg = f"Error: The name '{alias}' already exists. Please choose a different name."
+                    send_length = str(len(error_msg)).encode(FORMAT)
+                    send_length += b' ' * (HEADER - len(send_length))
+                    conn.send(send_length)
+                    conn.send(error_msg.encode(FORMAT))
+                else:
+                    name = True
+                    client_alias[addr] = alias
+                    print(f"[REGISTERED] {addr} registered as {alias}. Welcome, {alias}!")
+                    confirmation_message = f"Successfully registered as {alias}."
+                    send_length = str(len(confirmation_message)).encode(FORMAT)
+                    send_length += b' ' * (HEADER - len(send_length))
+                    conn.send(send_length)
+                    conn.send(confirmation_message.encode(FORMAT))
+            elif msg.startswith("/store"):
+                filename = msg.split(" ")[1]
+                f = open(filename, "r")
+                print(f"[{addr}] {format_time}: Uploaded a file \"{filename}\"")
+                print(f"File Content: {f.read()}")
+            elif msg.startswith("/get"):
+                filename = msg.split(" ")[1]
+                if os.path.exists(filename):
+                    with open(filename, 'r') as file:
+                        file_content = file.read()
+                        send_length = str(len(file_content)).encode(FORMAT)
+                        send_length += b' ' * (HEADER - len(send_length))
+                        conn.send(send_length)
+                        conn.send(file_content.encode(FORMAT))
+                        print(f"[FILE SENT] {filename} to {addr}")
+                else:
+                    print(f"File '{filename}' does not exist.")
+            elif msg == DIRECTORY:
+                files = os.listdir('.')
+                files_list = "\n".join(files)
+                send_length = str(len(files_list)).encode(FORMAT)
+                send_length += b' ' * (HEADER - len(send_length))
+                conn.send(send_length)
+                conn.send(files_list.encode(FORMAT))
+                print(f"[DIRECTORY LIST] to {addr}")
+            else:
+                if name:
+                    print(f"[{alias}] {msg}")
+                else:
+                    alias = client_alias.get(addr, "Unknown")
+                    print(f"[{addr}] {msg}")
 
     conn.close()
 
@@ -39,7 +95,7 @@ def start():
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args =(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        print(f"[CLIENT APP OPENED] {threading.active_count() - 1}")
 
 
 
